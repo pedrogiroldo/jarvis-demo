10876d9fe1fb5bad1baa0a025e94ecf5b9798f7f
Pedro Andrade Giroldo <pedrogiroldo40@gmail.com>
2025-11-13 18:19:15 -0300

    add agent

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AudioModule } from './audio/audio.module';
+import { AgentModule } from './agent/agent.module';
 
 @Module({
-  imports: [AudioModule],
+  imports: [AudioModule, AgentModule],
   controllers: [AppController],
   providers: [AppService],
 })
